e26d90b47d80b7d7899ae24d5af7847bd49a2b5c
Aleh Kuis <pestleralex@gmail.com>
2025-02-09 11:44:22 +0300

    fea: change 404 page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const NotFoundPage: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1
         style={{
           color: 'red',
-          fontSize: '24px',
+          fontSize: '44px',
           fontWeight: 'bold',
           textAlign: 'center',
           marginTop: '50px',
@@ -14,6 +17,19 @@ const NotFoundPage: React.FC = () => {
       >
         404 - Page Not Found
       </h1>
+      <button
+        className="btn"
+        onClick={() => navigate(-1)}
+        style={{
+          display: 'block',
+          margin: '20px auto',
+          padding: '10px 20px',
+          fontSize: '16px',
+          cursor: 'pointer',
+        }}
+      >
+        Назад
+      </button>
     </div>
   );
 };
