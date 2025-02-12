ed91e14efd3fbc8654205e5263cff44cc9585486
Aleh Kuis <pestleralex@gmail.com>
2025-02-12 12:21:01 +0300

    feat: refactoring

--- src/components/card-details/CardDetails.tsx
+++ src/components/card-details/CardDetails.tsx
@@ -18,15 +18,10 @@ const CardDetails: React.FC<CardDetailsProps> = ({ person }) => {
         CLOSE
       </button>
       <h2>{person.name}</h2>
+      <p>Gender: {person.gender}</p>
       <p>Height: {person.height}</p>
       <p>Mass: {person.mass}</p>
       <p>Hair Color: {person.hair_color}</p>
-      <p>Skin Color: {person.skin_color}</p>
-      <p>Eye Color: {person.eye_color}</p>
-      <p>Birth Year: {person.birth_year}</p>
-      <p>Gender: {person.gender}</p>
-      <p>Created: {person.created}</p>
-      <p>Edited: {person.edited}</p>
     </div>
   );
 };
