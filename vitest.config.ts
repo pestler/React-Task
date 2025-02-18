0ad994727b6fda01aff2ae34efbea227872b7c0f
Aleh Kuis <pestleralex@gmail.com>
2025-02-18 17:47:18 +0300

    feat: add sass loader

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/variables.scss";`,
+      },
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
