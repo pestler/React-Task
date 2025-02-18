022e4076df75822ebfb7b2d708723d46cfa0de9d
Aleh Kuis <pestleralex@gmail.com>
2025-02-18 17:49:50 +0300

    fix: fix error

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,13 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/styles/variables.scss";`,
-      },
-    },
-  },
   test: {
     globals: true,
     environment: 'jsdom',
